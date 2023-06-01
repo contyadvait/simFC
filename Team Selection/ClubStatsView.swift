28ffbe1206ee90f40b219757ff83c37e09578f3e
Advait "Conty" Contractor <advaitthepro@outlook.com>
2023-06-01 23:24:48 +0800

    Added gauges for AMD but still broken

--- Team Selection/ClubStatsView.swift
+++ Team Selection/ClubStatsView.swift
@@ -31,7 +31,25 @@ struct ClubStatsView: View {
                     Label("E. Haaland, 97, ST/CF, PL TOTS", systemImage: "figure.soccer")
                     Label("K. De Bruyne, 97, CM/CAM, PL TOTS", systemImage: "arrow.turn.up.left")
                     Label("R. Dias, 97, CB, PL TOTS", systemImage: "trophy")
-                    Label("Ederson M., 97, GK, FUT Ballers", systemImage: "hand.raised")
+                    Label("Ederson M., 90, GK, FUT Ballers", systemImage: "hand.raised")
+                }
+                Section("Overall") {
+                    HStack {
+                        Spacer()
+                        Gauge(value: Double(97/100)) {
+                            Text("Attack")
+                        }
+                        .gaugeStyle(.accessoryCircularCapacity)
+                        Gauge(value: Double(97/100)) {
+                            Text("Midfield")
+                        }
+                        .gaugeStyle(.accessoryCircularCapacity)
+                        Gauge(value: Double((90+97)/100)) {
+                            Text("Defence")
+                        }
+                        .gaugeStyle(.accessoryCircularCapacity)
+                        Spacer()
+                    }
                 }
             }
         }
